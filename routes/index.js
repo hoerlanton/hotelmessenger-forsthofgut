493e62f4eac408fb2c4980c303465fb43275f68a
Anton Hörl <anton.hoerl@gmx.at>
2017-07-17 11:52:23 +0200

    Made split properly line 150 index.js

--- routes/index.js
+++ routes/index.js
@@ -117,7 +117,9 @@ router.post('/guestsMessage', function(req, res, next){
                     //If a new file got attached, also send the attachment
                     if(uploadedFileName !== undefined && newFileUploaded === true) {
                         console.log("sendbroadcastfile runned");
-                        message.text += " Datei angehängt: " + uploadedFileName[uploadedFileName.length - 1].split("*");
+                        var uploadedFileNameSplitted = uploadedFileName.split("*");
+                        var uploadedFileWithoutNumber = [uploadedFileNameSplitted.length - 1];
+                        message.text += " Datei angehängt: " + uploadedFileWithoutNumber;
                         sourceFile.sendBroadcastFile(forsthofgutGaeste[i].senderId, URLUploadedFile);
                     }
                 }
