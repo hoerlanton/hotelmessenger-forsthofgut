00dd2654f61ddd28f92ce9bcb8068193df4ed942
Anton Hörl <anton.hoerl@gmx.at>
2017-08-04 16:31:00 +0200

    Object assign snippet added to vendor.bundle.js

--- public/vendor.bundle.js
+++ public/vendor.bundle.js
@@ -5,6 +5,28 @@ webpackJsonp([4],{
 
 "use strict";
 
+        if (typeof Object.assign != 'function') {
+            Object.assign = function(target) {
+                'use strict';
+                if (target == null) {
+                    throw new TypeError('Cannot convert undefined or null to object');
+                }
+
+                target = Object(target);
+                for (var index = 1; index < arguments.length; index++) {
+                    var source = arguments[index];
+                    if (source != null) {
+                        for (var key in source) {
+                            if (Object.prototype.hasOwnProperty.call(source, key)) {
+                                target[key] = source[key];
+                            }
+                        }
+                    }
+                }
+                return target;
+            };
+        }
+
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
